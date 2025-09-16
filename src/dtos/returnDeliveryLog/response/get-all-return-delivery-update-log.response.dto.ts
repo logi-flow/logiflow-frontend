8544b84394575395dc726660611f1ce99cb5f864
kohyeokjae <kohj0686@naver.com>
2025-09-16 12:12:02 +0900

    feat: return-delivery-log-dto

---
+++ src/dtos/returnDeliveryLog/response/get-all-return-delivery-update-log.response.dto.ts
@@ -0,0 +1,9 @@
+export interface GetAllReturnDeliveryUpdateLogResponseDto {
+  id: number;
+  returnDeliveryId: number;
+  username: string;
+  type: string;
+  prevData: string;
+  newData: string;
+  createdAt: string;
+}
