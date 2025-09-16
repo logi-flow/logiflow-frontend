cf010ee77a7d85388e96951a94eb4fb9f3cd05ab
mijinn0 <pmj_0818@naver.com>
2025-09-16 12:29:59 +0900

    feat: 수당 항목 api 생성

---
+++ src/apis/allowanceType/allowance-type-log.apis.ts
@@ -0,0 +1,25 @@
+import type { AxiosError } from "axios";
+import type { GetAllowanceTypeUpdateLogResponseDto } from "../../dtos/allowanceTypeLog/response/get-allowance-type-update-log.response.dto";
+import type PageDto from "../../dtos/page.dto";
+import type ResponseDto from "../../dtos/response.dto";
+import { axiosInstance, bearerAuthorization, responseErrorHandler, responseSuccessHandler } from "../axios-config";
+import { GET_ALLOWANCE_TYPE_UPDATE_LOGS_URL } from "./allowance-type.urls";
+
+export const getAllowanceTypeUpdateLogs = async (
+  page: number,
+  size: number,
+  sort: string,
+  accessToken: string
+): Promise<ResponseDto<PageDto<GetAllowanceTypeUpdateLogResponseDto>>> => {
+  try {
+    const response = await axiosInstance.get(
+      GET_ALLOWANCE_TYPE_UPDATE_LOGS_URL, {
+        params: { page, size, sort },
+        ...bearerAuthorization(accessToken)
+      }
+    );
+    return responseSuccessHandler(response);
+  } catch (error) {
+    return responseErrorHandler(error as AxiosError<ResponseDto>);
+  }
+};
---
+++ src/apis/constants.ts
@@ -0,0 +1,3 @@
+const API_DOMAIN = import.meta.env.REACT_APP_DOMAIN || `http://localhost:8080`;
+
+export const API_BASE = `${API_DOMAIN}/api/v1`;
