2b457a13cc466c156bbc433535448032a21dbcc5
mijinn0 <pmj_0818@naver.com>
2025-10-13 19:58:21 +0900

    refactor: 출근부 관련 페이지 헤더 및 사이드바 반영

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import ContractListPage from './pages/contract/ContractListPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import CreateContractPage from './pages/contract/CreateContractPage'
+import AllAttendanceListPage from './pages/attendance/AllAttendanceListPage'
+import AttendanceRegisterPage from './pages/attendance/AttendanceRegisterPage'
+import MyAttendanceListPage from './pages/attendance/MyAttendanceListPage'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -11,6 +14,9 @@ createRoot(document.getElementById('root')!).render(
       <Route path="/" element={<App />} />
       <Route path="/contracts" element={<ContractListPage />} />
       <Route path="/contracts/create" element={<CreateContractPage />} />
+      <Route path="/attendances/list" element={<AllAttendanceListPage />} />
+      <Route path="/attendances/me/register" element={<AttendanceRegisterPage />} />
+      <Route path="/attendances/me/list" element={<MyAttendanceListPage />} />
     </Routes>
   </BrowserRouter>,
 )
